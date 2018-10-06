87e3a033a4c1d2cef505ddc1ec33a2a2272ccae1
AndersUOlsson <olsson.anders@protonmail.com>
2018-10-06 21:09:48 +0200

    Add project files.

---
+++ App1/ViewController.cs
@@ -0,0 +1,109 @@
+﻿
+using AudioToolbox;
+using CoreLocation;
+using MapKit;
+using System;
+using UIKit;
+
+namespace IntervallRun
+{
+    public partial class ViewController : UIViewController
+    {
+        CLLocationManager locationManager =   new CLLocationManager();
+        SystemSound systemSound           =   new SystemSound(1003);
+        CLLocation location               =   new CLLocation();
+        
+        //public static CLLocationManager Manager { get; set; }
+
+        public double MaxValue             { get; private set; }
+        public double MinValue             { get; private set; }
+        private bool StartFlag             { get; set; }
+        public double Speed                { get; set; }
+        private double MaxMinPerKm         { get; set; }
+        private double MinMinPerKm         { get; set; }
+
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad();
+            map = new MKMapView(View.Bounds)
+            {
+                AutoresizingMask = UIViewAutoresizing.FlexibleDimensions
+            };
+
+
+            //This resize map to iphone boundery
+            // View.AddSubview(map);
+
+            //Take a look at UIKit.UIView.SizeThatFits
+            locationManager.RequestWhenInUseAuthorization();
+            locationManager.RequestAlwaysAuthorization();
+            map.ShowsUserLocation = true;
+
+            map.DidUpdateUserLocation += (s, ergs) =>
+            {
+                if (map.UserLocation != null)
+                {
+                    CLLocationCoordinate2D coords = map.UserLocation.Coordinate;
+                    MKCoordinateSpan span = new MKCoordinateSpan(0.01, 0.01);
+                    map.Region = new MKCoordinateRegion(coords, span);
+                    Speed = map.UserLocation.Location.Speed;
+
+                    if(StartFlag)
+                    {
+                        
+                        ControllTheRunnersSpeed(MaxValue, MinValue, Speed);
+                    }
+                }
+            };
+
+            //Initialize max speed stepper, this is config for setting max speed on run.
+            maxSpeedStepper.MaximumValue = 150.0;
+            maxSpeedStepper.MinimumValue = 0.0;
+            maxSpeedStepper.Value = 3.9 * 10;
+            maxSpeedStepper.ValueChanged += (sender, args) =>
+            {
+                MaxValue = maxSpeedStepper.Value / 10.0;
+                maxSpeedLabel.Text = MaxValue.ToString();
+            };
+
+            //Initialize max speed stepper, this is config for setting max speed on run.
+            minSpeedStepper.MaximumValue = 150.0;
+            minSpeedStepper.MinimumValue = 0.0;
+            minSpeedStepper.Value = 3.9 * 10;
+            minSpeedStepper.ValueChanged += (sender, args) =>
+            {
+                MinValue = minSpeedStepper.Value / 10.0;
+                minSpeedLabel.Text = MinValue.ToString();
+            };
+
+            StartFlag = false;
+            startButton.TouchUpInside += (object sender, System.EventArgs e) =>
+            {
+                StartFlag = !StartFlag;
+                Console.WriteLine("You have pressed the button");
+            };
+        }
+
+        private void ControllTheRunnersSpeed(double maxValue, double minValue, double currentSpeed)
+        {
+            Console.WriteLine("This is the speed" + currentSpeed);
+
+            if (maxValue > currentSpeed)
+            {
+                Console.WriteLine("You are runnig to fast, SLOW down. MAX Value" + maxValue);
+            }
+            else if(minValue < currentSpeed)
+            {
+                Console.WriteLine("You are running to slow, SPEED up, MIN Value" + minValue);
+            }
+            else
+            {
+                Console.WriteLine("You are running in a perfect speed");
+            }
+        }
+    }
+}
---
+++ App1/ViewController.designer.cs
@@ -0,0 +1,83 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace IntervallRun
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UISlider destinationSlider { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        MapKit.MKMapView map { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel maxSpeedLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIStepper maxSpeedStepper { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel minSpeedLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIStepper minSpeedStepper { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton startButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (destinationSlider != null) {
+                destinationSlider.Dispose ();
+                destinationSlider = null;
+            }
+
+            if (map != null) {
+                map.Dispose ();
+                map = null;
+            }
+
+            if (maxSpeedLabel != null) {
+                maxSpeedLabel.Dispose ();
+                maxSpeedLabel = null;
+            }
+
+            if (maxSpeedStepper != null) {
+                maxSpeedStepper.Dispose ();
+                maxSpeedStepper = null;
+            }
+
+            if (minSpeedLabel != null) {
+                minSpeedLabel.Dispose ();
+                minSpeedLabel = null;
+            }
+
+            if (minSpeedStepper != null) {
+                minSpeedStepper.Dispose ();
+                minSpeedStepper = null;
+            }
+
+            if (startButton != null) {
+                startButton.Dispose ();
+                startButton = null;
+            }
+        }
+    }
+}
